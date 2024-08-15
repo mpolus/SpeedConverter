cf741f362dac604df6a91f3b27d1630af07c6e53
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-08-15 12:59:26 -0400

    hb

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class SpeedConverter {
+public class SpeedConverter {
+    public static void main(String[] args) {
+        System.out.println(toMPH(1.5));
+        System.out.println(toMPH(10.25));
+        System.out.println(toMPH(-5.6));
+        System.out.println(toMPH(25.42));
+        System.out.println(toMPH(75.114));
+
+
+    }
+
+    public static long toMPH(double kilometersPerHour){
+        if(kilometersPerHour < 0){
+            return -1;
+        } else{
+            double mph = kilometersPerHour / 1.609;
+
+            return Math.round(mph);
+        }
+
+    }
 }
